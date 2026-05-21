6d9fb0751c3c9442c58cfaeac506e5d47db711a6
seungmin cha <108518794+blueid13@users.noreply.github.com>
2026-05-21 16:01:36 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // 예: "https://script.google.com/macros/s/AKfycb.../exec"
 window.ECG_REVIEW_CONFIG = {
   APP_VERSION: "1.1.0-simple",
-  APPS_SCRIPT_URL: "PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE"
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwIfJNVOt_E2UpuOMR0g0CzKbVPFclGFTgxGlI5_LdnbOXMwxlX200WAcoxRABGAB-w/exec"
 };
